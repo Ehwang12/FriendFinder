62e88e329c9c34a60cfc66ae930c8f8678e22572
Esther <estshwang@gmail.com>
2019-05-31 21:38:09 -0500

    working app functionality across files

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,7 +1,36 @@
+var friends = require("../app/data/friends.js");
+
+module.exports = function(app) {
 //show all possible matches
-app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "/app/data/friends.js"));
-  });
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+      });
+
+    app.post("/api/friends", function(req, res) {
+        var totalDiff = 0;
+        vr bestMatch = {
+            name: "",
+            photo: "",
+            friendDiff: 1000
+        }
+    })
+
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
+
+    var b = userScores.map(function(item) {
+        return parseInt(item, 10);
+    })
+
+    userData = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: b
+    }
+}
+
+
 
 //retreiving data from user
 app.post("/api/friends", function(req, res) {
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,9 +1,15 @@
-//GET route to survey page
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/survey.html"));
-});
+var path = require("path");
 
+module.exports = function(app) {
+
+    //GET route to survey page
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname + "/routing/survey.html"));
+    });
+  
 //route to home page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/home.html"));
-});
+app.use("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/routing/home.html"));
+    });
+}
+
